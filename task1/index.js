07b71c0b10beef89bc8bda7eac82e14fdf20b840
Nika <nikax193@gmail.com>
2026-03-16 15:17:14 +0400

    add task1

---
+++ task1/index.js
@@ -0,0 +1,34 @@
+// var , let, const, 
+// condole.log 
+// alert;
+
+
+
+// let misalmeba = 'gamrjoba';
+
+// let num = 17;
+
+// console.log("Hello", misalmeba, num);
+
+
+
+function add(a, b) {
+    return a + b
+}
+
+console.log(">>>>>", add(2, 3))
+
+
+const add2 =(a, b) => a + b;
+
+const add3 =(a, b) => a - b;
+
+const add4 =(a, b) => a * b;
+
+const add5 =(a, b) => a/b;
+
+
+console.log("<>", add2(10, 5));
+console.log("<>", add3(10, 5));
+console.log("<>", add4(10, 5));
+console.log("<>", add5(10, 5));
