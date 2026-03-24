e8feed672f19392fd1aa535bbe78bd2a4d138ad1
Nika <nikax193@gmail.com>
2026-03-25 00:45:51 +0400

    add new task3

---
+++ task2/test.js
@@ -0,0 +1,46 @@
+let qula = 75;
+    qula = 60;
+    qula = 91;
+
+
+let shefaseba ;
+
+if (qula >= 91) {
+    shefaseba = "A"
+}
+else if(qula >= 81) {
+    shefaseba = "B"
+}
+else if(qula >= 71) {
+    shefaseba = "C"
+}
+else if(qula >= 61) {
+    shefaseba = "D"
+}
+else if(qula >= 51) {
+    shefaseba = "E"
+}
+else console.log("chaiwei")
+
+console.log("shefaseba: <>:",shefaseba)
+
+
+
+let asaki = 2;
+while (asaki <= 20) {
+    console.log(asaki);
+    asaki++;
+}
+console.log("========")
+
+console.log(asaki);
+
+console.log("--------")
+do {
+    console.log(asaki);
+    asaki++;
+} while(asaki <= 30);
+
+console.log("+++++++++")
+
+console.log(asaki);
---
+++ task3/main.js
@@ -0,0 +1,10 @@
+//objeqts arry
+
+const { jsxs } = require("react/jsx-runtime");
+
+const num = [2, 3, 5];
+
+
+num.push(7)
+
+console.log(num)
